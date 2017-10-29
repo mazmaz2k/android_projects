b88005359a2eb4133aa49d050a44194ea683fe8b
Zvukamar <fre3run@gmail.com>
2017-10-29 18:44:30 +0200

    Finish Lab2

--- app/src/main/java/com/example/alex/lab2/MainActivity.java
+++ app/src/main/java/com/example/alex/lab2/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.alex.lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    Button btn;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        btn = (Button) findViewById(R.id.init_button);
+        text = (TextView) findViewById(R.id.name_textView);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                text.setTextSize(progress + 20);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seekBar.setProgress(0);
+            }
+        });
     }
 }
