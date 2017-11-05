e0965b729277caa7bfb22d0bd37945764b90a5af
Zvukamar <fre3run@gmail.com>
2017-11-05 19:36:20 +0200

    Added share text from another intent

--- app/src/main/java/com/example/alex/lab3/MainActivity.java
+++ app/src/main/java/com/example/alex/lab3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.alex.lab3;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn;
     EditText text;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btn = (Button) findViewById(R.id.submitBtn);
         text = (EditText) findViewById(R.id.editText);
-
+        intent = getIntent();
+        String s = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if(s != null) {
+            text.setText(s);
+        }
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
