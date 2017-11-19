f69da27533ad081f368ab4fa87d5ca13f93b953e
Zvukamar <fre3run@gmail.com>
2017-11-19 17:58:33 +0200

    Added stuff from lab4

---
+++ app/src/main/java/com/example/alex/lab5/DBHelper.java
@@ -0,0 +1,25 @@
+package com.example.alex.lab5;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+
+    public DBHelper(Context context) {
+        super(context, Constants.DB_NAME, null, Constants.VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + Constants.Course.TABLE_NAME + " (" + Constants.Course.COURSE_ID +" NUMBER PRIMARY KEY, " + Constants.Course.COURSE_NAME + " TEXT, " + Constants.Course.COURSE_GRADE + " NUMBER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + Constants.Course.TABLE_NAME);
+        onCreate(db);
+    }
+}
