9742ee6b6636989bf1a32480ec0da5be200f77f9
Omri Mizrahi <mazmaz2k@gmail.com>
2017-12-31 20:03:34 +0200

    Finish

---
+++ app/src/main/java/axeleration/com/lab10/MainActivity.java
@@ -0,0 +1,30 @@
+package axeleration.com.lab10;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final int CODE_REQUEST = 1234;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CALL_PHONE} , CODE_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
