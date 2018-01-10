ce5c9c68724763139850618ff93d88c504c46966
Zvukamar <fre3run@gmail.com>
2018-01-10 20:32:07 +0200

    Added comments

--- app/src/main/java/axeleration/com/ex4_homework/ListFragment.java
+++ app/src/main/java/axeleration/com/ex4_homework/ListFragment.java
@@ -20,21 +20,21 @@ public class ListFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_list, container, false);
-        ListView listView = view.findViewById(R.id.listView);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        View view = inflater.inflate(R.layout.fragment_list, container, false); // convert fragment to view.
+        ListView listView = view.findViewById(R.id.listView);   // list view.
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() { // set onclick on each item from list view.
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                FragmentManager manager = getFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                Fragment fragment = new DataFragment();
-                fragment.setArguments(setUpBundle());
-                if(getActivity().findViewById(R.id.data) != null)
+                FragmentManager manager = getFragmentManager(); //  get the fragment manager.
+                FragmentTransaction transaction = manager.beginTransaction();   // get the fragment transaction.
+                Fragment fragment = new DataFragment(); // New date fragment.
+                fragment.setArguments(setUpBundle());   // set arguments to the fragment.
+                if(getActivity().findViewById(R.id.data) != null)   // if its tablet mode - landscape.
                     transaction.replace(R.id.data, fragment);
-                else
+                else    // its mobile mode - portrait.
                     transaction.replace(R.id.listLayout, fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
+                transaction.addToBackStack(null);   // add to the stack on back functionality.
+                transaction.commit();   // commit the fragment transaction.
             }
         });
         MyAdapter adapter = new MyAdapter(getActivity(), ((MainActivity)getActivity()).getCursor());
@@ -43,14 +43,14 @@ public class ListFragment extends Fragment {
     }
 
     private Bundle setUpBundle() {
-        Cursor cursor = ((MainActivity)getActivity()).getCursor();
-        Bundle bundle = new Bundle();
+        Cursor cursor = ((MainActivity)getActivity()).getCursor();  // get the cursor from main activity.
+        Bundle bundle = new Bundle();       // new bundle.
         String contactName = (cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
         String phoneNumber = (cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
         String imageUri = (cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI)));
-        bundle.putString("name", contactName);
-        bundle.putString("phone", phoneNumber);
-        bundle.putString("img", imageUri);
+        bundle.putString("name", contactName);  // set name to the bundle.
+        bundle.putString("phone", phoneNumber); // set phone to the bundle.
+        bundle.putString("img", imageUri);  // set image to the bundle.
         return bundle;
     }
 
